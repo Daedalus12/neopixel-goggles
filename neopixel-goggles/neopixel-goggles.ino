9f2824b5e3648ea0e727f429b44fd11978dbee06
daedalus12 <Daedalus12@gmail.com>
2017-09-14 14:59:32 -0700

    Convert to HSI, color pulse fixed pin

--- neopixel-goggles/neopixel-goggles.ino
+++ neopixel-goggles/neopixel-goggles.ino
@@ -1,3 +1,4 @@
+//#include "ColorPulse.h"
 #include <Adafruit_NeoPixel.h>
 
 #define PIN A0
@@ -19,31 +20,67 @@ color red = strip.Color(255, 0, 0);
 color green = strip.Color(0, 255, 0);
 color blue = strip.Color(0, 0, 255);
 
-const uint8_t MIN_BRIGHTNESS = 63;
+const uint8_t MIN_BRIGHTNESS = 127;
 const uint8_t MAX_BRIGHTNESS = 255;
 
-const uint16_t cosN = 600;
-uint8_t cosCurve[cosN];
+const uint16_t cosN = 127;
 uint16_t i = 0;
+
+uint32_t hsi2rgb(float H, float S, float I) {
+    uint8_t r, g, b;
+    H = fmod(H, 360); // cycle H around to 0-360 degrees
+    H = 3.14159*H / (float)180; // Convert to radians.
+    S = S>0 ? (S<1 ? S : 1) : 0; // clamp S and I to interval [0,1]
+    I = I>0 ? (I<1 ? I : 1) : 0;
+
+    // Math! Thanks in part to Kyle Miller.
+    if (H < 2.09439) {
+        r = 255 * I / 3 * (1 + S*cos(H) / cos(1.047196667 - H));
+        g = 255 * I / 3 * (1 + S*(1 - cos(H) / cos(1.047196667 - H)));
+        b = 255 * I / 3 * (1 - S);
+    }
+    else if (H < 4.188787) {
+        H = H - 2.09439;
+        g = 255 * I / 3 * (1 + S*cos(H) / cos(1.047196667 - H));
+        b = 255 * I / 3 * (1 + S*(1 - cos(H) / cos(1.047196667 - H)));
+        r = 255 * I / 3 * (1 - S);
+    }
+    else {
+        H = H - 4.188787;
+        b = 255 * I / 3 * (1 + S*cos(H) / cos(1.047196667 - H));
+        r = 255 * I / 3 * (1 + S*(1 - cos(H) / cos(1.047196667 - H)));
+        g = 255 * I / 3 * (1 - S);
+    }
+
+    return ((uint32_t)r << 16) | ((uint32_t)g << 8) | b;
+}
+
+template<class T>
+trigScale(T& ret, const float& val, const T& valMin, const T& valMax) {
+    ret = valMin + 0.5*(valMax - valMin)*(1 + val);
+}
 
 void setup() {
     strip.begin();
     strip.setBrightness(255); // should only be called once
     strip.show();
-
-    for (uint16_t j = 0; j < cosN; ++j) {
-        cosCurve[j] = MIN_BRIGHTNESS + 0.5*(MAX_BRIGHTNESS - MIN_BRIGHTNESS)*(1 + cos(PI * 2 * j / cosN));        
-    }
 }
 
 void loop() {
-    setColor(strip.Color(cosCurve[i], cosCurve[i], cosCurve[i]));
+    double intensity, saturation;
+    float val = cos(i * 2.0 * PI / cosN);
+    trigScale(intensity, val, 0.2, 1.0);
+    trigScale(saturation, val, 0.0, 1.0);
+
+    setColor(hsi2rgb(0, 0, intensity));
+    strip.setPixelColor(0, hsi2rgb(0, saturation, intensity));
     strip.show();
-    delayMicroseconds(16);
     ++i;
     if (i == cosN) {
         i = 0;
     }
+
+    delay(14);
 }
 
 void setColor(uint32_t c) {
@@ -51,106 +88,4 @@ void setColor(uint32_t c) {
         strip.setPixelColor(i, c);
     }
     strip.show();
-}
-
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-    for (uint16_t i = 0; i<strip.numPixels(); i++) {
-        strip.setPixelColor(i, c);
-        strip.show();
-        delay(wait);
-    }
-}
-
-void rainbow(uint8_t wait) {
-    uint16_t i, j;
-
-    for (j = 0; j<256; j++) {
-        for (i = 0; i<strip.numPixels(); i++) {
-            strip.setPixelColor(i, Wheel((i + j) & 255));
-        }
-        strip.show();
-        delay(wait);
-    }
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-    uint16_t i, j;
-
-    for (j = 0; j<256 * 5; j++) { // 5 cycles of all colors on wheel
-        for (i = 0; i< strip.numPixels(); i++) {
-            strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-        }
-        strip.show();
-        delay(wait);
-    }
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-    if (WheelPos < 85) {
-        return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-    }
-    else if (WheelPos < 170) {
-        WheelPos -= 85;
-        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-    }
-    else {
-        WheelPos -= 170;
-        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-    }
-}
-
-/**
-*      ^   ^   ^
-* ~~~~~ ColorWave ~~~~~
-*        V   V   V
-*/
-void colorWave(uint8_t wait) {
-    int i, j, stripsize, cycle;
-    float ang, rsin, gsin, bsin, offset;
-
-    static int tick = 0;
-
-    stripsize = strip.numPixels();
-    cycle = stripsize * 25; // times around the circle...
-
-    while (++tick % cycle) {
-        offset = map2PI(tick);
-
-        for (i = 0; i < stripsize; i++) {
-            ang = map2PI(i) - offset;
-            rsin = sin(ang);
-            gsin = sin(2.0 * ang / 3.0 + map2PI(int(stripsize / 6)));
-            bsin = sin(4.0 * ang / 5.0 + map2PI(int(stripsize / 3)));
-            strip.setPixelColor(i, strip.Color(trigScale(rsin), trigScale(gsin), trigScale(bsin)));
-        }
-
-        strip.show();
-        delay(wait);
-    }
-
-}
-
-/**
-* Scale a value returned from a trig function to a byte value.
-* [-1, +1] -> [0, 254]
-* Note that we ignore the possible value of 255, for efficiency,
-* and because nobody will be able to differentiate between the
-* brightness levels of 254 and 255.
-*/
-byte trigScale(float val) {
-    val += 1.0; // move range to [0.0, 2.0]
-    val *= 127.0; // move range to [0.0, 254.0]
-
-    return int(val) & 255;
-}
-
-/**
-* Map an integer so that [0, striplength] -> [0, 2PI]
-*/
-float map2PI(int i) {
-    return PI*2.0*float(i) / float(strip.numPixels());
 }
