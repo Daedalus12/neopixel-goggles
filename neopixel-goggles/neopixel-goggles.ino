00e11fac7c6cc706bad8c44a9b66d1d220c33c5c
daedalus12 <Daedalus12@gmail.com>
2017-09-23 16:46:19 -0700

    Get basic pulsing to voice working

--- neopixel-goggles/neopixel-goggles.ino
+++ neopixel-goggles/neopixel-goggles.ino
@@ -1,12 +1,11 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 3
-#define STRIPSIZE 16
+const byte PIN = 3;
+const byte STRIPSIZE = 16;
 
-const int audioSampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
-unsigned int audioSample;
-
-
+const unsigned int audioSampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
+const unsigned int audioMemoryLength = 5 * (1000/audioSampleWindow);
+unsigned int audioMemory[audioMemoryLength];
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
 // Parameter 3 = pixel type flags, add together as needed:
@@ -55,7 +54,7 @@ byte saturations[STRIPSIZE];
 
 void setup() {
     strip.begin();
-    strip.setBrightness(255); // should only be called once
+    strip.setBrightness(100); // should only be called once
     strip.show();
 
     for (byte k = 0; k < STRIPSIZE; ++k)
@@ -63,23 +62,24 @@ void setup() {
         saturations[k] = 0;
         hues[k] = 0;
     }
-    Serial.begin(9600);
 
+    Serial.begin(9600);
+    for (unsigned int i = 0; i < audioMemoryLength; ++i) {
+        audioMemory[i] = 50;
+    }
 }
 
 const uint16_t nCycles = 63;
 const byte cycleTime = 15;
 uint16_t i = 0;
 int brightness;
+int audioSample;
 
 void loop() {
     double intensity, saturation;
     float val = cos(i * 2.0 * PI / nCycles + PI);
     trigScale(intensity, val, 0.35, 1.0);
     trigScale(saturation, val, 0.0, 1.0);
-
-    for (byte k = 0; k < STRIPSIZE; ++k)
-        strip.setPixelColor(k, hsi2rgb(hues[k], saturations[k]*saturation, brightness));
 
     unsigned long startMillis = millis();  // Start of sample window
     unsigned int peakToPeak = 0;   // peak-to-peak level
@@ -90,7 +90,8 @@ void loop() {
     // collect data for 50 mS
     while (millis() - startMillis < audioSampleWindow)
     {
-        audioSample = analogRead(1);
+        audioSample = analogRead(2);
+        //Serial.println(audioSample);
         if (audioSample < 1024)  // toss out spurious readings
         {
             if (audioSample > signalMax)
@@ -103,29 +104,53 @@ void loop() {
             }
         }
     }
-    peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
-    double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
-    brightness = map(peakToPeak, 0, 1023, 0, 255);
-    Serial.println(brightness);
-
-    strip.show();
-    ++i;
-    if (i == nCycles) {
-        i = 0;
-        for (byte k = 0; k < STRIPSIZE; ++k)
-        {
-            if (random(2) == 0) {
-                saturations[k] = 1;
-                hues[k] = random(180, 270);
-            } else {
-                saturations[k] = 0;
-                hues[k] = 0;
-            }
-        }
-
+    unsigned int minV = 1024;
+    unsigned int maxV = 0;
+    for (unsigned int i = 0; i < audioMemoryLength - 1; ++i) {
+        minV = min(minV, audioMemory[i + 1]);
+        maxV = max(maxV, audioMemory[i + 1]);
+        audioMemory[i] = audioMemory[i + 1];
     }
 
-    delay(cycleTime);
+    peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+    minV = min(minV, peakToPeak);
+    maxV = max(maxV, peakToPeak);
+    audioMemory[audioMemoryLength - 1] = peakToPeak;
+
+    byte b = constrain(map(peakToPeak, minV, maxV, 0, 255), 0, 255);
+
+    Serial.println("");
+    Serial.print(peakToPeak);
+    Serial.print("\t");
+    Serial.print(minV);
+    Serial.print("\t");
+    Serial.print(maxV);
+    Serial.print("\t");
+    Serial.print(b);
+    Serial.println("");
+
+
+    //++i;
+    //if (i == nCycles) {
+    //    i = 0;
+    //    for (byte k = 0; k < STRIPSIZE; ++k)
+    //    {
+    //        if (random(2) == 0) {
+    //            saturations[k] = 1;
+    //            hues[k] = random(180, 270);
+    //        } else {
+    //            saturations[k] = 0;
+    //            hues[k] = 0;
+    //        }
+    //    }
+    //}
+
+    for (byte k = 0; k < 16; ++k) {
+        strip.setPixelColor(k, hsi2rgb(0, 1, (float)b/255.0f));
+    }
+    strip.show();
+
+    delay(10);
 }
 
 void setColor(uint32_t c) {
